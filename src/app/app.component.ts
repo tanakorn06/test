bddc8d892639d485a932709f587e240747568010
tanakorn06 <boonsukurtbew@gmail.com>
2021-11-16 17:29:29 +0700

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,48 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+
+export class AppComponent {
+  user = { id: 1001, name:"Tanakorn 1.",address:"Thailand"}
+
+  i:number = 0;
+  nameInput:string="";
+  sayHi(){
+    return "Hi from " + this.user.name;
+  }
+
+  addCount(){
+    this.i++;
+  }
+  disCount(){
+    this.i--;
+  }
+
+  readName(name:string){
+    console.log(name);
+    this.nameInput=name;
+  }
+
+  valid:boolean = 1<2;
+  validAge:boolean=false;
+
+  readAge(age:string){
+    let a = parseInt(age)
+    this.validAge = a>=18 && a<=25
+  }
+
+  menus = ["Shabu","KFC","MK","BBQ","Salad"];
+
+  courses= [
+    { id:1, title:"TypeScript"},
+    { id:2, title:"Java"},
+    { id:3, title:"PHP"},
+    { id:4, title:"Angular"},
+    { id:5, title:"Flutter"}
+  ]
+
+}
